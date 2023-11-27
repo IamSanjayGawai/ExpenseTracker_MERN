55fdba555f987cb16e227b7e19c2e0e20b4a6a94
Sanjay Gawai <sanjaygawai2022@gmail.comm>
2023-11-27 17:13:58 +0530

    Create user  Schemas and  Model

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const monggose = require('mongoose');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const colors = require('colors');
@@ -12,28 +12,20 @@ connectDB();
 
 const app = express();
 
-
-
-
 //middlewares
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-
-
 
 //default route
 app.get('/', (req, res)=>{
     res.send('hello world');
 })
 
-
 //port 
-
 const PORT  = process.env.PORT || 5000;
 
 //listen server
-
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT}`)
 })
